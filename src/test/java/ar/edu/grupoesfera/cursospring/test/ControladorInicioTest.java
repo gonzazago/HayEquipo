8a195ddc0f20877c0c0c03837ca3d36af7cf9040
tdortona <tdortona@gmail.com>
2016-11-24 10:27:16 -0300

    Arreglo de bug en test y agregado de buscarCanchaRetorneVistaBusquedad

--- src/test/java/ar/edu/grupoesfera/cursospring/test/ControladorInicioTest.java
+++ src/test/java/ar/edu/grupoesfera/cursospring/test/ControladorInicioTest.java
@@ -1,9 +1,11 @@
 package ar.edu.grupoesfera.cursospring.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -13,6 +15,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.grupoesfera.cursospring.controladores.ControladorInicio;
+import ar.edu.grupoesfera.cursospring.modelo.Cancha;
+import ar.edu.grupoesfera.cursospring.servicios.CanchasServicios;
 
 public class ControladorInicioTest {
 
@@ -23,14 +27,11 @@ public class ControladorInicioTest {
 		ModelMap modelMock = mock(ModelMap.class);
 		HttpSession httpMock = mock(HttpSession.class);
 		
-		httpMock.setAttribute("idUsuario", 1);
-		httpMock.setAttribute("usuario", null);
-		
 		when(requestMock.getSession()).thenReturn(httpMock);
 		when(httpMock.getAttribute("idUsuario")).thenReturn((long)1);
 		when(httpMock.getAttribute("usuario")).thenReturn(null);
 		
-		String map = controlador.unirsePartido(anyLong(), modelMock, requestMock);		
+		String map = controlador.unirsePartido(2L, modelMock, requestMock);		
 		
 		assertThat(map).isEqualTo("redirect:/iniciarSesion");
 	}
@@ -53,5 +54,31 @@ public class ControladorInicioTest {
 		ModelAndView map = controlador.registroUsuario(modelMock);
 		
 		assertThat(map.getViewName()).isEqualTo("registrarse");
+	}
+	
+	@Test
+	public void buscarCanchaRetorneVistaBusquedad(){
+		ControladorInicio controlador = new ControladorInicio();
+		CanchasServicios canchasMock = mock(CanchasServicios.class);
+		
+		List<Cancha> listCanchasTest = new ArrayList<Cancha>();
+		
+		Cancha canchaTest = new Cancha();
+		canchaTest.setIdCancha((long)1);
+		canchaTest.setNombre("nomTest");
+		canchaTest.setProvincia("provTest");
+		canchaTest.setLocalidad("locTest");
+		canchaTest.setCalle("calleTest");
+		canchaTest.setNumero(1);
+		canchaTest.setCanchasDisponibles(1);
+		
+		listCanchasTest.add(canchaTest);
+		
+		when(canchasMock.buscarCanchas("")).thenReturn(listCanchasTest);
+		controlador.setCanchasServicios(canchasMock);
+		
+		ModelAndView map = controlador.buscarCancha("");
+		
+		assertThat(map.getViewName()).isEqualTo("busquedad");
 	}
 }
